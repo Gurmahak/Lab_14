aa89a863e6e3f3521efe6459788949cc7d4c1fc0
Gurmahak <gurmahak94@gmail.com>
2019-10-07 18:22:06 -0700

    initial commit

---
+++ src/main/java/com/example/lab_5/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.lab_5;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.loadUrl("file:///android_asset/UI.html");
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = ((TextView) findViewById(R.id.editText)).getText().toString();
+                webView.evaluateJavascript("addNode("+ input +")", num -> {
+                    Log.d("WEBVIEW", "returned from webview is: " + num);
+                });
+                Snackbar.make(view, "Number Of Cards Drawn: " + input , Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        WebView webView = findViewById(R.id.webView);
+        webView.reload();
+        /*Toast.makeText(getApplicationContext(),
+                "This is a message displayed in a Toast",
+                Toast.LENGTH_SHORT).show();*/
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
